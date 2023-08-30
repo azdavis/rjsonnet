0a222f8cf46cc21c74a550847ccb68f0214ab1f0
Ariel Davis <ariel.z.davis@icloud.com>
2023-08-30 00:13:20 -0700

    Doc some tokens

--- crates/jsonnet-syntax/build.rs
+++ crates/jsonnet-syntax/build.rs
@@ -3,9 +3,12 @@ use rustc_hash::FxHashMap;
 fn main() {
   let trivia = ["Whitespace", "SlashSlashComment", "HashComment", "BlockComment", "Invalid"];
   let grammar = include_str!("syntax.ungram");
+  let doc =
+    code_h2_md_map::get(include_str!("../../docs/tokens.md"), |tok| format!("Token: `{tok}`\n"));
+  let doc: FxHashMap<_, _> = doc.iter().map(|(&k, v)| (k, v.as_str())).collect();
   let mut special = FxHashMap::<&str, &str>::default();
   special.insert("Id", "an identifier");
   special.insert("Number", "a number");
   special.insert("String", "a string");
-  syntax_gen::gen("Jsonnet", &trivia, grammar, &FxHashMap::default(), &special)
+  syntax_gen::gen("Jsonnet", &trivia, grammar, &doc, &special)
 }
---
+++ docs/tokens.md
@@ -0,0 +1,11 @@
+# Tokens
+
+These are some tokens in Jsonnet.
+
+## `{`
+
+Open an object.
+
+## `}`
+
+The companion of `{`.
