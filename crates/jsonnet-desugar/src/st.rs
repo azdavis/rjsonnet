e7c9cceab9787cc038de2ea087e8cc021dc8acb9
Ariel Davis <ariel.z.davis@icloud.com>
2023-09-26 20:57:24 -0700

    Add some helpers for errors

--- crates/jsonnet-desugar/src/st.rs
+++ crates/jsonnet-desugar/src/st.rs
@@ -1,8 +1,11 @@
 use jsonnet_hir::Arenas;
+use jsonnet_syntax::{ast::AstNode, kind::SyntaxToken};
+use text_size::TextRange;
 
 #[derive(Debug, Default)]
 pub(crate) struct St {
   arenas: Arenas,
+  errors: Vec<(TextRange, &'static str)>,
 }
 
 impl St {
@@ -12,6 +15,21 @@ impl St {
 
   pub(crate) fn expr(&mut self, e: jsonnet_hir::ExprData) -> jsonnet_hir::ExprMust {
     self.arenas.expr.alloc(e)
+  }
+
+  pub(crate) fn err<N>(&mut self, node: &N, msg: &'static str)
+  where
+    N: AstNode,
+  {
+    self.err_(node.syntax().text_range(), msg);
+  }
+
+  pub(crate) fn err_token(&mut self, tok: SyntaxToken, msg: &'static str) {
+    self.err_(tok.text_range(), msg);
+  }
+
+  fn err_(&mut self, range: TextRange, msg: &'static str) {
+    self.errors.push((range, msg));
   }
 
   pub(crate) fn finish(self) -> Arenas {
