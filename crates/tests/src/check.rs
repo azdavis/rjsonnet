7dff45db82d8d0d1df8e8904c80c8a2f6d5c8d05
Ariel Davis <ariel.z.davis@icloud.com>
2023-10-18 11:31:41 +0200

    Rename jsonnet_dynamics -> jsonnet_exec

--- crates/tests/src/check.rs
+++ crates/tests/src/check.rs
@@ -7,7 +7,7 @@ pub(crate) fn pass(s: &str) {
   jsonnet_statics::check(&mut st, &cx, &desugar.arenas, desugar.top);
   let statics_errors = st.finish();
   let env = jsonnet_val::Env::default();
-  let eval = jsonnet_dynamics::eval(&env, &desugar.arenas, desugar.top);
+  let eval = jsonnet_exec::eval(&env, &desugar.arenas, desugar.top);
   assert!(lex.errors.is_empty());
   assert!(parse.errors.is_empty());
   assert!(desugar.errors.is_empty());
