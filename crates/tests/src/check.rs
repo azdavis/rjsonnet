7ac5e6aadfa09f6d5d278dd72f27a3d14b0a91ca
Ariel Davis <ariel.z.davis@icloud.com>
2023-10-26 11:05:22 +0200

    Swap, rename

--- crates/tests/src/check.rs
+++ crates/tests/src/check.rs
@@ -1,18 +1,18 @@
 pub(crate) fn pass(s: &str) {
   let lex = jsonnet_lex::get(s);
+  assert!(lex.errors.is_empty());
   let parse = jsonnet_parse::get(&lex.tokens);
+  assert!(parse.errors.is_empty());
   let desugar = jsonnet_desugar::get(parse.root);
+  assert!(desugar.errors.is_empty());
   let mut st = jsonnet_statics::St::default();
   let cx = jsonnet_statics::Cx::default();
   jsonnet_statics::check(&mut st, &cx, &desugar.arenas, desugar.top);
   let statics_errors = st.finish();
+  assert!(statics_errors.is_empty());
   let env = jsonnet_eval::val::Env::default();
   let exec = jsonnet_eval::exec::get(&env, &desugar.arenas, desugar.top);
-  let val = exec.unwrap();
-  let manifest = jsonnet_eval::manifest::get(&desugar.arenas, val);
-  assert!(lex.errors.is_empty());
-  assert!(parse.errors.is_empty());
-  assert!(desugar.errors.is_empty());
-  assert!(statics_errors.is_empty());
+  let exec = exec.unwrap();
+  let manifest = jsonnet_eval::manifest::get(&desugar.arenas, exec);
   assert!(manifest.is_ok());
 }
