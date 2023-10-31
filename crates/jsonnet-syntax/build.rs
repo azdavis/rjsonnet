c7b1a4b620dc70d0d9e4a3f7b527877c64f883b0
Ariel Davis <ariel.z.davis@icloud.com>
2023-10-30 20:47:05 -0700

    Use an array for special tokens

--- crates/jsonnet-syntax/build.rs
+++ crates/jsonnet-syntax/build.rs
@@ -6,9 +6,10 @@ fn main() {
   let doc =
     code_h2_md_map::get(include_str!("../../docs/tokens.md"), |tok| format!("Token: `{tok}`\n"));
   let doc: FxHashMap<_, _> = doc.iter().map(|(&k, v)| (k, v.as_str())).collect();
-  let mut special = FxHashMap::<&str, &str>::default();
-  special.insert("Id", "an identifier");
-  special.insert("Number", "a number");
-  special.insert("String", "a string");
+  let special = FxHashMap::from_iter([
+    ("Id", "an identifier"),
+    ("Number", "a number"),
+    ("String", "a string"),
+  ]);
   syntax_gen::gen("Jsonnet", &trivia, grammar, &doc, &special)
 }
