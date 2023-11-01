a88d67fd2ada8c1ee1bb20ef01a2c02d5c100f58
Ariel Davis <ariel.z.davis@icloud.com>
2023-11-01 14:45:24 -0700

    Up to new syntax-gen

--- crates/jsonnet-syntax/build.rs
+++ crates/jsonnet-syntax/build.rs
@@ -3,19 +3,24 @@
 use rustc_hash::FxHashMap;
 
 fn main() {
-  let trivia = ["Whitespace", "SlashSlashComment", "HashComment", "BlockComment", "Invalid"];
-  let grammar = include_str!("syntax.ungram");
   let doc =
     code_h2_md_map::get(include_str!("../../docs/tokens.md"), |tok| format!("Token: `{tok}`\n"));
   let doc: FxHashMap<_, _> = doc.iter().map(|(&k, v)| (k, v.as_str())).collect();
-  let special = FxHashMap::from_iter([
-    ("Id", "an identifier"),
-    ("Number", "a number"),
-    ("DoubleQuotedString", "a double-quoted string"),
-    ("SingleQuotedString", "a single-quoted string"),
-    ("DoubleQuotedVerbatimString", "a double-quoted verbatim string"),
-    ("SingleQuotedVerbatimString", "a single-quoted verbatim string"),
-    ("TextBlock", "a text block"),
-  ]);
-  syntax_gen::gen("Jsonnet", &trivia, grammar, &doc, &special)
+  let options = syntax_gen::Options {
+    lang: "Jsonnet",
+    trivia: &["Whitespace", "SlashSlashComment", "HashComment", "BlockComment", "Invalid"],
+    grammar: include_str!("syntax.ungram"),
+    doc: &doc,
+    special: &FxHashMap::from_iter([
+      ("Id", "an identifier"),
+      ("Number", "a number"),
+      ("DoubleQuotedString", "a double-quoted string"),
+      ("SingleQuotedString", "a single-quoted string"),
+      ("DoubleQuotedVerbatimString", "a double-quoted verbatim string"),
+      ("SingleQuotedVerbatimString", "a single-quoted verbatim string"),
+      ("TextBlock", "a text block"),
+    ]),
+    file: file!(),
+  };
+  syntax_gen::gen(&options);
 }
