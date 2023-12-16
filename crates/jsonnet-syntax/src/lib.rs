ce053c151d35e63b52cdbd50a40d69583caf8b41
Ariel Davis <ariel.z.davis@icloud.com>
2023-12-16 01:49:04 -0500

    Ignore

--- crates/jsonnet-syntax/src/lib.rs
+++ crates/jsonnet-syntax/src/lib.rs
@@ -2,10 +2,12 @@
 
 #![allow(missing_debug_implementations, missing_docs)]
 
+#[allow(clippy::pedantic, missing_debug_implementations, missing_docs)]
 pub mod ast {
   include!(concat!(env!("OUT_DIR"), "/ast.rs"));
 }
 
+#[allow(clippy::pedantic, missing_debug_implementations, missing_docs)]
 pub mod kind {
   include!(concat!(env!("OUT_DIR"), "/kind.rs"));
 }
