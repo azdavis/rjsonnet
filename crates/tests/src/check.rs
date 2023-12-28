b5680f8c7c60d58973340454e8c016890b8f4523
Ariel Davis <ariel.z.davis@icloud.com>
2023-12-27 20:55:09 -0800

    Use Artifacts

--- crates/tests/src/check.rs
+++ crates/tests/src/check.rs
@@ -1,8 +1,11 @@
-use jsonnet_desugar::Desugar;
 use jsonnet_eval::val::{json, jsonnet};
 use jsonnet_expr::{Number, Prim};
 
-fn exec(s: &str) -> (Desugar, jsonnet_eval::error::Result<jsonnet::Val>) {
+struct Artifacts {
+  desugar: jsonnet_desugar::Desugar,
+}
+
+fn exec(s: &str) -> (Artifacts, jsonnet_eval::error::Result<jsonnet::Val>) {
   let lex = jsonnet_lex::get(s);
   if let Some(e) = lex.errors.first() {
     panic!("lex error: {e}");
@@ -27,21 +30,22 @@ fn exec(s: &str) -> (Desugar, jsonnet_eval::error::Result<jsonnet::Val>) {
   let empty = jsonnet::Object::default();
   let cx = jsonnet_eval::exec::Cx::new(&env, &empty);
   let val = jsonnet_eval::exec::get(cx, &desugar.arenas, desugar.top);
-  (desugar, val)
+  let art = Artifacts { desugar };
+  (art, val)
 }
 
-fn manifest_raw(s: &str) -> (Desugar, json::Val) {
-  let (desugar, val) = exec(s);
+fn manifest_raw(s: &str) -> (Artifacts, json::Val) {
+  let (art, val) = exec(s);
   let empty = jsonnet::Object::default();
   let val = match val {
     Ok(x) => x,
-    Err(e) => panic!("exec error: {}", e.display(&desugar.arenas.str)),
+    Err(e) => panic!("exec error: {}", e.display(&art.desugar.arenas.str)),
   };
-  let val = match jsonnet_eval::manifest::get(&desugar.arenas, &empty, val) {
+  let val = match jsonnet_eval::manifest::get(&art.desugar.arenas, &empty, val) {
     Ok(x) => x,
-    Err(e) => panic!("manifest error: {}", e.display(&desugar.arenas.str)),
+    Err(e) => panic!("manifest error: {}", e.display(&art.desugar.arenas.str)),
   };
-  (desugar, val)
+  (art, val)
 }
 
 fn from_serde(ar: &jsonnet_expr::StrArena, serde: serde_json::Value) -> json::Val {
@@ -74,20 +78,20 @@ fn from_serde(ar: &jsonnet_expr::StrArena, serde: serde_json::Value) -> json::Va
 
 /// tests that `jsonnet` execution results in an error whose message is `want`.
 pub(crate) fn exec_err(jsonnet: &str, want: &str) {
-  let (desugar, a) = exec(jsonnet);
+  let (art, a) = exec(jsonnet);
   let err = a.expect_err("no error");
-  let got = err.display(&desugar.arenas.str).to_string();
+  let got = err.display(&art.desugar.arenas.str).to_string();
   assert_eq!(want, got.as_str());
 }
 
 /// tests that `jsonnet` manifests to the `json`.
 pub(crate) fn manifest(jsonnet: &str, json: &str) {
   let want: serde_json::Value = serde_json::from_str(json).unwrap();
-  let (desugar, got) = manifest_raw(jsonnet);
-  let want = from_serde(&desugar.arenas.str, want);
+  let (art, got) = manifest_raw(jsonnet);
+  let want = from_serde(&art.desugar.arenas.str, want);
   if want != got {
-    let want = want.display(&desugar.arenas.str);
-    let got = got.display(&desugar.arenas.str);
+    let want = want.display(&art.desugar.arenas.str);
+    let got = got.display(&art.desugar.arenas.str);
     panic!("want: {want}\ngot:  {got}");
   }
 }
@@ -97,14 +101,16 @@ pub(crate) fn manifest_self(s: &str) {
   manifest(s, s);
 }
 
-/// tests that `jsonnet` manifests to the string `want`. NOTE: `want` is NOT interpreted as JSON.
+/// tests that `jsonnet` manifests to the string `want`.
+///
+/// NOTE: `want` is NOT interpreted as JSON.
 pub(crate) fn manifest_str(jsonnet: &str, want: &str) {
-  let (mut desugar, got) = manifest_raw(jsonnet);
-  let want = desugar.arenas.str.str(want.to_owned().into_boxed_str());
+  let (mut art, got) = manifest_raw(jsonnet);
+  let want = art.desugar.arenas.str.str(want.to_owned().into_boxed_str());
   let json::Val::Prim(Prim::String(got)) = got else { panic!("did not get a String") };
   if want != got {
-    let want = desugar.arenas.str.get(&want);
-    let got = desugar.arenas.str.get(&got);
+    let want = art.desugar.arenas.str.get(&want);
+    let got = art.desugar.arenas.str.get(&got);
     panic!("want: {want:?}\ngot:  {got:?}")
   }
 }
