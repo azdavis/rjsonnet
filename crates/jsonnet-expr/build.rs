6625bc919d069f9f9649d8fd9d9c655503082a9e
Ariel Davis <ariel.z.davis@icloud.com>
2023-12-30 15:59:36 -0800

    Rearrange str/id build

--- crates/jsonnet-expr/build.rs
+++ crates/jsonnet-expr/build.rs
@@ -1,74 +1,131 @@
-//! Generate some string/identifier name.
+//! Generate some string/identifier names.
 
 use quote::{format_ident, quote};
 use std::collections::HashSet;
 
 #[allow(clippy::too_many_lines)]
 fn main() {
-  let preset = [
-    ("STD", "std", true),
+  let identifiers = [
+    ("STD", "std"),
     // STD_UNUTTERABLE is the same as STD but it has a str that cannot be written in user code as an
     // id, so it will never be shadowed. it is used in desugaring.
-    ("STD_UNUTTERABLE", "$std", true),
-    ("SELF", "self", true),
-    ("SUPER", "super", true),
+    ("STD_UNUTTERABLE", "$std"),
+    ("SELF", "self"),
+    ("SUPER", "super"),
     // OUTER_SELF and OUTER_SUPER are also unutterable and are used in the desugaring.
-    ("OUTER_SELF", "$outerself", true),
-    ("OUTER_SUPER", "$outersuper", true),
-    ("DOLLAR", "$", true),
-    ("JOIN", "join", false),
-    ("MAKE_ARRAY", "makeArray", false),
-    ("LENGTH", "length", false),
-    ("SLICE", "slice", false),
-    ("MOD", "mod", false),
-    ("EQUALS", "equals", false),
-    ("OBJECT_HAS_EX", "objectHasEx", false),
-    ("PARAMETER_NOT_BOUND", "Parameter not bound", false),
-    ("ASSERTION_FAILED", "Assertion failed", false),
-    ("TODO", "TODO", false),
+    ("OUTER_SELF", "$outerself"),
+    ("OUTER_SUPER", "$outersuper"),
+    ("DOLLAR", "$"),
   ];
+  let strings = [
+    ("JOIN", "join"),
+    ("MAKE_ARRAY", "makeArray"),
+    ("LENGTH", "length"),
+    ("SLICE", "slice"),
+    ("MOD", "mod"),
+    ("EQUALS", "equals"),
+    ("OBJECT_HAS_EX", "objectHasEx"),
+    ("PARAMETER_NOT_BOUND", "Parameter not bound"),
+    ("ASSERTION_FAILED", "Assertion failed"),
+    ("TODO", "TODO"),
+  ];
+
   let mut names = HashSet::<&'static str>::new();
   let mut contents = HashSet::<&'static str>::new();
-  for &(name, content, _) in &preset {
+  for &(name, content) in identifiers.iter().chain(strings.iter()) {
     assert!(names.insert(name), "duplicate name: {name}");
     assert!(contents.insert(content), "duplicate content: {content}");
   }
   drop(names);
   drop(contents);
-  let str_idx_constants = preset.iter().enumerate().map(|(idx, &(name, _, make_id))| {
-    let name = format_ident!("{name}");
-    let idx = u32::try_from(idx).unwrap();
-    let vis = if make_id {
-      quote! {}
-    } else {
-      quote! { pub }
-    };
-    quote! { #vis const #name: Self = Self(#idx); }
-  });
-  let str_idx_debug_arms = preset.iter().enumerate().map(|(idx, &(_, contents, _))| {
-    let idx = u32::try_from(idx).unwrap();
-    quote! { #idx => d.field(&#contents), }
-  });
-  let str_constants = preset.iter().filter_map(|&(name, _, make_id)| {
-    if make_id {
-      return None;
+
+  let identifiers_and_strings = {
+    let str_idx_constants = std::iter::empty()
+      .chain(identifiers.iter().map(|x| (x, false)))
+      .chain(strings.iter().map(|x| (x, true)))
+      .enumerate()
+      .map(|(idx, (&(name, _), is_pub))| {
+        let name = format_ident!("{name}");
+        let idx = u32::try_from(idx).unwrap();
+        let vis = if is_pub {
+          quote! { pub }
+        } else {
+          quote! {}
+        };
+        quote! { #vis const #name: Self = Self(#idx); }
+      });
+    let str_idx_debug_arms =
+      identifiers.iter().chain(strings.iter()).enumerate().map(|(idx, &(_, contents))| {
+        let idx = u32::try_from(idx).unwrap();
+        quote! { #idx => d.field(&#contents), }
+      });
+    let capacity = identifiers.len() + strings.len();
+    let str_arena_inserts = identifiers.iter().chain(strings.iter()).map(|&(name, contents)| {
+      let name = format_ident!("{name}");
+      quote! { assert_eq!(StrIdx::#name, ret.mk_idx(bs(#contents))); }
+    });
+
+    quote! {
+      impl StrIdx {
+        #(#str_idx_constants)*
+      }
+
+      impl fmt::Debug for StrIdx {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+          let mut d = f.debug_tuple("StrIdx");
+          match self.to_u32() {
+            #(#str_idx_debug_arms)*
+            n => d.field(&n),
+          };
+          d.finish()
+        }
+      }
+
+      fn bs(s: &str) -> Box<str> {
+        s.to_owned().into_boxed_str()
+      }
+
+      impl Default for StrArena {
+        fn default() -> Self {
+          let mut ret = Self {
+            idx_to_contents: Vec::with_capacity(#capacity),
+            contents_to_idx: FxHashMap::default(),
+          };
+          #(#str_arena_inserts)*
+          ret
+        }
+      }
     }
-    let name = format_ident!("{name}");
-    Some(quote! { pub const #name: Self = Self(StrRepr::Idx(StrIdx::#name)); })
-  });
-  let id_constants = preset.iter().filter_map(|&(name, _, make_id)| {
-    if !make_id {
-      return None;
+  };
+
+  let identifiers_only = {
+    let constants = identifiers.iter().map(|&(name, _)| {
+      let name = format_ident!("{name}");
+      quote! { pub const #name: Self = Self(StrIdx::#name); }
+    });
+
+    quote! {
+      impl Id {
+        #(#constants)*
+      }
     }
-    let name = format_ident!("{name}");
-    Some(quote! { pub const #name: Self = Self(StrIdx::#name); })
-  });
-  let str_arena_inserts = preset.iter().map(|&(name, contents, _)| {
-    let name = format_ident!("{name}");
-    quote! { assert_eq!(StrIdx::#name, ret.mk_idx(bs(#contents))); }
-  });
-  let preset_len = preset.len();
+  };
+
+  let strings_only = {
+    let constants = strings.iter().map(|&(name, _)| {
+      let name = format_ident!("{name}");
+      quote! { pub const #name: Self = Self(StrRepr::Idx(StrIdx::#name)); }
+    });
+
+    quote! {
+      impl Str {
+        #(#constants)*
+      }
+    }
+  };
+
   let file = file!();
+
   let contents = quote! {
     pub const _GENERATED_BY: &str = #file;
 
@@ -76,43 +133,11 @@ fn main() {
     use rustc_hash::FxHashMap;
     use std::fmt;
 
-    impl StrIdx {
-      #(#str_idx_constants)*
-    }
+    #identifiers_and_strings
 
-    impl fmt::Debug for StrIdx {
-      fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut d = f.debug_tuple("StrIdx");
-        match self.to_u32() {
-          #(#str_idx_debug_arms)*
-          n => d.field(&n),
-        };
-        d.finish()
-      }
-    }
+    #identifiers_only
 
-    impl Str {
-      #(#str_constants)*
-    }
-
-    impl Id {
-      #(#id_constants)*
-    }
-
-    fn bs(s: &str) -> Box<str> {
-      s.to_owned().into_boxed_str()
-    }
-
-    impl Default for StrArena {
-      fn default() -> Self {
-        let mut ret = Self {
-          idx_to_contents: Vec::with_capacity(#preset_len),
-          contents_to_idx: FxHashMap::default(),
-        };
-        #(#str_arena_inserts)*
-        ret
-      }
-    }
+    #strings_only
   };
   write_rs_tokens::go(contents, "generated.rs");
 }
