e3c662ce9ff5053f083306c5416a8f3047b2a2c9
Ariel Davis <ariel.z.davis@icloud.com>
2024-02-05 20:37:06 -0800

    Use a block

--- crates/jsonnet-ls/src/server.rs
+++ crates/jsonnet-ls/src/server.rs
@@ -8,14 +8,16 @@ pub(crate) struct Server {
 impl Server {
   pub(crate) fn init(init: lsp_types::InitializeParams) -> Self {
     let mut ret = Self { st: jsonnet_analyze::St::default(), fs: paths::RealFileSystem::default() };
-    let url = init.root_uri.expect("root uri");
-    let root_path = convert::path_buf(&url).expect("root path");
-    let wd = walkdir::WalkDir::new(root_path.as_path());
-    let paths = wd.into_iter().filter_map(|entry| {
-      let entry = entry.ok()?;
-      entry.path().extension().is_some_and(|x| x == "jsonnet").then(|| entry.into_path())
-    });
-    let paths = paths.collect();
+    let paths: Vec<_> = {
+      let url = init.root_uri.expect("root uri");
+      let root_path = convert::path_buf(&url).expect("root path");
+      let wd = walkdir::WalkDir::new(root_path.as_path());
+      let iter = wd.into_iter().filter_map(|entry| {
+        let entry = entry.ok()?;
+        entry.path().extension().is_some_and(|x| x == "jsonnet").then(|| entry.into_path())
+      });
+      iter.collect()
+    };
     ret.st.update_many(&ret.fs, Vec::new(), paths);
     ret
   }
