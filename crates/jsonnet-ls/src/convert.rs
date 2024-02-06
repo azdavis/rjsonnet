edfd894bc8b610a57cfb934ce4945dadd759f519
Ariel Davis <ariel.z.davis@icloud.com>
2024-02-05 20:34:04 -0800

    Rm dead

--- crates/jsonnet-ls/src/convert.rs
+++ crates/jsonnet-ls/src/convert.rs
@@ -2,16 +2,6 @@ use crate::server::Server;
 use anyhow::{bail, Result};
 use lsp_types::Url;
 
-// pub(crate) fn canonical_path_buf<F>(fs: &F, url: &Url) -> Result<paths::CanonicalPathBuf>
-// where
-//   F: paths::FileSystem,
-// {
-//   match url.to_file_path() {
-//     Ok(pb) => Ok(fs.canonicalize(pb.as_path())?),
-//     Err(()) => bail!("couldn't make a URL into a file path: {url}"),
-//   }
-// }
-
 pub(crate) fn path_buf(url: &Url) -> Result<std::path::PathBuf> {
   if url.scheme() != "file" {
     bail!("not a file url: {url}")
