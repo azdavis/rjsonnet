45dd5ccb71ebece663371e3a43074904ba6cc9af
Ariel Davis <ariel.z.davis@icloud.com>
2024-02-16 01:46:01 -0800

    Use PathMap

--- crates/lang-srv/src/server.rs
+++ crates/lang-srv/src/server.rs
@@ -3,14 +3,13 @@ use always::always;
 use anyhow::{bail, Result};
 use lsp_types::Url;
 use paths::FileSystem as _;
-use rustc_hash::FxHashMap;
 
 #[derive(Debug, Default)]
 pub(crate) struct Server {
   pub(crate) fs: paths::RealFileSystem,
   pub(crate) queue: lsp_server::ReqQueue<(), ()>,
   pub(crate) file_watch: bool,
-  pub(crate) open_files: FxHashMap<paths::PathId, String>,
+  pub(crate) open_files: paths::PathMap<String>,
 }
 
 impl Server {
