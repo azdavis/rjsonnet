0bd76a5a69279d7086ed15ca8e0d9c4149568f69
Ariel Davis <ariel.z.davis@icloud.com>
2024-10-03 13:38:42 -0700

    Move get call to a mod

--- crates/jsonnet-statics/src/check.rs
+++ crates/jsonnet-statics/src/check.rs
@@ -1,9 +1,11 @@
 //! Check expressions for static validity: variables are in scope, types match up, etc.
 
+mod call;
+
 use crate::{error, st, ty};
 use always::always;
 use jsonnet_expr::def::{self, Def};
-use jsonnet_expr::{BinaryOp, Expr, ExprArena, ExprData, ExprMust, Id, Prim, UnaryOp};
+use jsonnet_expr::{BinaryOp, Expr, ExprArena, ExprData, Id, Prim, UnaryOp};
 use rustc_hash::{FxHashMap, FxHashSet};
 use std::collections::BTreeSet;
 
@@ -135,7 +137,7 @@ pub(crate) fn get(st: &mut st::St<'_>, ar: &ExprArena, expr: Expr) -> ty::Ty {
           }
         }
       }
-      get_call(st, expr, *func, func_ty, &positional_tys, &named_tys)
+      call::get(st, expr, *func, func_ty, &positional_tys, &named_tys)
     }
     ExprData::Id(id) => {
       if let Some((ty, def)) = st.get(*id) {
@@ -279,58 +281,6 @@ pub(crate) fn get(st: &mut st::St<'_>, ar: &ExprArena, expr: Expr) -> ty::Ty {
   // huge problem.
   st.insert_expr_ty(expr, ret);
   ret
-}
-
-fn get_call(
-  st: &mut st::St<'_>,
-  expr: ExprMust,
-  fn_expr: Expr,
-  fn_ty: ty::Ty,
-  positional: &[(Expr, ty::Ty)],
-  named: &FxHashMap<Id, (Expr, ty::Ty)>,
-) -> ty::Ty {
-  match st.data(fn_ty).clone() {
-    ty::Data::Fn(ty::Fn::Regular(fn_data)) => {
-      let positional_iter = fn_data.params.iter().zip(positional.iter());
-      for (param, &(arg, ty)) in positional_iter {
-        st.unify(arg.unwrap_or(expr), param.ty, ty);
-      }
-      let mut named = named.clone();
-      for param in fn_data.params.iter().skip(positional.len()) {
-        match named.remove(&param.id) {
-          Some((arg, ty)) => st.unify(arg.unwrap_or(expr), param.ty, ty),
-          None => {
-            if param.required {
-              st.err(fn_expr.unwrap_or(expr), error::Kind::MissingArgument(param.id, param.ty));
-            }
-          }
-        }
-      }
-      for (idx, &(arg, _)) in positional.iter().enumerate().skip(fn_data.params.len()) {
-        st.err(arg.unwrap_or(expr), error::Kind::ExtraPositionalArgument(idx + 1));
-      }
-      for (id, (arg, _)) in named {
-        st.err(arg.unwrap_or(expr), error::Kind::ExtraNamedArgument(id));
-      }
-      fn_data.ret
-    }
-    ty::Data::Fn(ty::Fn::Std(std_fn)) => {
-      log::warn!("TODO: get std call {std_fn}");
-      ty::Ty::ANY
-    }
-    ty::Data::Union(tys) => {
-      // must be compatible with ALL of the union parts.
-      let ret_tys =
-        tys.into_iter().map(|fn_ty| get_call(st, expr, fn_expr, fn_ty, positional, named));
-      let ret_ty = ty::Data::Union(ret_tys.collect());
-      st.get_ty(ret_ty)
-    }
-    ty::Data::Prim(ty::Prim::Any) => ty::Ty::ANY,
-    ty::Data::Prim(_) | ty::Data::Array(_) | ty::Data::Object(_) => {
-      st.err(expr, error::Kind::CallNonFn(fn_ty));
-      ty::Ty::ANY
-    }
-  }
 }
 
 fn define_binds(
---
+++ crates/jsonnet-statics/src/check/call.rs
@@ -0,0 +1,56 @@
+//! Checking function calls.
+
+use crate::{error, st, ty};
+use jsonnet_expr::{Expr, ExprMust, Id};
+use rustc_hash::FxHashMap;
+
+pub(crate) fn get(
+  st: &mut st::St<'_>,
+  expr: ExprMust,
+  fn_expr: Expr,
+  fn_ty: ty::Ty,
+  positional: &[(Expr, ty::Ty)],
+  named: &FxHashMap<Id, (Expr, ty::Ty)>,
+) -> ty::Ty {
+  match st.data(fn_ty).clone() {
+    ty::Data::Fn(ty::Fn::Regular(fn_data)) => {
+      let positional_iter = fn_data.params.iter().zip(positional.iter());
+      for (param, &(arg, ty)) in positional_iter {
+        st.unify(arg.unwrap_or(expr), param.ty, ty);
+      }
+      let mut named = named.clone();
+      for param in fn_data.params.iter().skip(positional.len()) {
+        match named.remove(&param.id) {
+          Some((arg, ty)) => st.unify(arg.unwrap_or(expr), param.ty, ty),
+          None => {
+            if param.required {
+              st.err(fn_expr.unwrap_or(expr), error::Kind::MissingArgument(param.id, param.ty));
+            }
+          }
+        }
+      }
+      for (idx, &(arg, _)) in positional.iter().enumerate().skip(fn_data.params.len()) {
+        st.err(arg.unwrap_or(expr), error::Kind::ExtraPositionalArgument(idx + 1));
+      }
+      for (id, (arg, _)) in named {
+        st.err(arg.unwrap_or(expr), error::Kind::ExtraNamedArgument(id));
+      }
+      fn_data.ret
+    }
+    ty::Data::Fn(ty::Fn::Std(std_fn)) => {
+      log::warn!("TODO: get std call {std_fn}");
+      ty::Ty::ANY
+    }
+    ty::Data::Union(tys) => {
+      // must be compatible with ALL of the union parts.
+      let ret_tys = tys.into_iter().map(|fn_ty| get(st, expr, fn_expr, fn_ty, positional, named));
+      let ret_ty = ty::Data::Union(ret_tys.collect());
+      st.get_ty(ret_ty)
+    }
+    ty::Data::Prim(ty::Prim::Any) => ty::Ty::ANY,
+    ty::Data::Prim(_) | ty::Data::Array(_) | ty::Data::Object(_) => {
+      st.err(expr, error::Kind::CallNonFn(fn_ty));
+      ty::Ty::ANY
+    }
+  }
+}
