8b0b7f478b719a90322133447ef1759cb0464af7
Ariel Davis <ariel.z.davis@icloud.com>
2024-10-04 21:45:52 -0700

    Add failing test

--- crates/tests/src/hover.rs
+++ crates/tests/src/hover.rs
@@ -15,3 +15,26 @@ fn arr_num() {
   )
   .check();
 }
+
+#[test]
+#[should_panic = "none of the lines were equal"]
+fn func_str_null() {
+  JsonnetInput::manifest(
+    r#"
+local f(x) =
+  if x == 1 then
+    "a"
+  else if x == 2 then
+    "b"
+  else
+    null;
+
+local xs = [null, f];
+##                ^ hover: (x: any) => null | string
+
+xs[0]
+"#,
+    "null",
+  )
+  .check();
+}
