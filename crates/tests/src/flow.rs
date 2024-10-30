b0f6ae728b287a4ca307beb879b010453a0d2105
Ariel Davis <ariel.z.davis@icloud.com>
2024-10-29 22:54:11 -0700

    Add failing test

--- crates/tests/src/flow.rs
+++ crates/tests/src/flow.rs
@@ -23,3 +23,23 @@ f(3) + f("hi")
   )
   .check();
 }
+
+#[test]
+#[should_panic = "none of the lines were equal"]
+fn not() {
+  JsonnetInput::manifest(
+    r#"
+local f(x) =
+##    ^ hover: (x: null | string) => string
+  assert x == null || std.isString(x);
+  if x != null && std.length(x) >= 10 then
+    x
+  else
+    "Hi"
+
+f(null)
+"#,
+    "Hi",
+  )
+  .check();
+}
