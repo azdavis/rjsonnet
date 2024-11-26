8fea0d2792cb00aeeef7216e6c11b39f0ded7e6a
Ariel Davis <ariel.z.davis@icloud.com>
2024-11-26 14:25:31 -0800

    Combine

--- crates/jsonnet-std-sig/src/tests.rs
+++ crates/jsonnet-std-sig/src/tests.rs
@@ -3,18 +3,14 @@
 use rustc_hash::FxHashSet;
 
 #[test]
-fn no_dupe() {
+fn ok() {
   let mut ident = FxHashSet::<&str>::default();
   let mut content = FxHashSet::<&str>::default();
+
   for f in super::FNS {
     assert!(ident.insert(f.name.ident()), "duplicate ident: {}", f.name.ident());
     assert!(content.insert(f.name.content()), "duplicate content: {}", f.name.content());
-  }
-}
 
-#[test]
-fn has_examples() {
-  for f in super::FNS {
     let has_enough = !f.examples.is_empty();
     let is_exempt = matches!(
       f.name.content(),
@@ -34,18 +30,13 @@ fn has_examples() {
         | "base64Decode"
         | "trace"
     );
-    let ok = has_enough != is_exempt;
+    let has_enough_when_not_exempt = has_enough != is_exempt;
     assert!(
-      ok,
+      has_enough_when_not_exempt,
       "either have examples and not exempt, or have no examples and exempt: `{}`",
       f.name.content()
     );
-  }
-}
 
-#[test]
-fn examples_match_up() {
-  for f in super::FNS {
     for example in f.examples {
       let want = format!("std.{}(", f.name.content());
       let ok = example.contains(&want) || (f.name.content() == "format" && example.contains('%'));
