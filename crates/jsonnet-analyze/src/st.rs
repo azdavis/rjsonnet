986b240b89eb302055d5e31d4c3b3e9ef29df331
Ariel Davis <ariel.z.davis@icloud.com>
2025-01-25 20:55:57 -0800

    Take mut

--- crates/jsonnet-analyze/src/st.rs
+++ crates/jsonnet-analyze/src/st.rs
@@ -710,8 +710,8 @@ impl lang_srv_state::State for St {
       const_eval::get(self, fs, path_id, Some(expr)),
       Some(const_eval::ConstEval::Std(None))
     );
+    let fields = self.with_fs.artifacts.statics.object_fields(ty)?;
     let wa = &self.with_fs.artifacts;
-    let fields = wa.statics.object_fields(ty)?;
     let fields = fields.into_iter().map(|(name, ty)| {
       let doc = if expr_is_std {
         jsonnet_expr::StdField::try_from(&name).ok().map(|x| x.doc().to_owned())
